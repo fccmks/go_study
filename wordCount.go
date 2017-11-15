b37dd54dc5fdd5c69ee720874bbbd75193ba027c
Moiseev Konstantin <moiseevko@mail.ru>
2017-11-16 00:31:47 +0700

    working word count

--- wordCount.go
+++ wordCount.go
@@ -7,14 +7,21 @@ import (
 
 func WordCount(s string) map[string]int {
 	wordMap := make(map[string]int)
-	for e := range strings.Fields(s) {
-
+	words := []string{}
+	count := 1
+	words = strings.Fields(s)
+	for i := 0; i < len(words); i++ {
+		_, ok := wordMap[words[i]]
+		if ok {
+			wordMap[words[i]] = count+1
+		} else {
+			wordMap[words[i]] = count
+		}
 	}
-}
+	return wordMap
+	}
 
 func main() {
 	//fmt.Printf("Words are: %q", strings.Fields("this is a string"))
-	words := []string{}
-	words = strings.Fields("hello e")
-	fmt.Println(words)
+	fmt.Println(WordCount("hi hi go he"))
 }
